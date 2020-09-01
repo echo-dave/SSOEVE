49ce88be39655bcd501182189ba2ab4b5f3ec1a5
Dave May <echo.dave@protonmail.com>
2020-09-01 08:34:11 -0400

    corrected paths of component imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, {useEffect} from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
-import Login from "./components/Login";
-import Services from "./components/Services";
+import Login from "./Pages/Login";
+import Services from "./Pages/Services";
 
 
 function App() {  
